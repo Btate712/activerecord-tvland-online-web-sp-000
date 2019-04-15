9e235a6d22ec74eb05daf43f82c15c0101391bed
Robert Tate <btate712@gmail.com>
2019-04-15 01:52:25 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_catchphrase_to_characters.rb
+++ db/migrate/006_add_catchphrase_to_characters.rb
@@ -1,5 +1,5 @@
-class AddCatchPhraseToCharacters < ActiveRecord::Migration[5.1]
+class AddCatchphraseToCharacters < ActiveRecord::Migration[5.1]
   def change
-    add_column :characters, :catch_phrase, :string
+    add_column :characters, :catchphrase, :string
   end
 end
